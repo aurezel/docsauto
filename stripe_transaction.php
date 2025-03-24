8d3d01453938e94035896078becc6fa87b85eaf7
elvis8020 <elvis8020@163.com>
2025-03-24 16:22:24 +0800

    t

--- stripe_transaction.php
+++ stripe_transaction.php
@@ -6,8 +6,8 @@
 	$stripe = new StripeService(STRIPE_SK);
 	$emails = $stripe->getTransactionsFromFile(CUSTOMER_EMAIL_FILE);
 	var_dump($emails);
-	if($_GET['act']){
-		$stripe->getTransactions($emails, $_GET['act']); //交易时间，0，1，2
+	if(isset($argv[1]){
+		$stripe->getTransactions($emails, intval($argv[1]); //交易时间，0，1，2
 	}else{
 		$stripe->getTransactions($emails); //交易时间，0，1，2
 	}
